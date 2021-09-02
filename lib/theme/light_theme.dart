721c872c6e9725638b2db62233a087394ba68147
handosme <handsome920112@gmail.com>
2021-09-02 01:54:22 -0700

    init commit

---
+++ lib/theme/light_theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+ThemeData light = ThemeData(
+  fontFamily: 'Roboto',
+  primaryColor: Color(0xFFEF7822),
+  secondaryHeaderColor: Color(0xFF1ED7AA),
+  disabledColor: Color(0xFFA0A4A8),
+  errorColor: Color(0xFFE84D4F),
+  brightness: Brightness.light,
+  accentColor: Color(0xFFEF7822),
+  hintColor: Color(0xFF9F9F9F),
+  cardColor: Colors.white,
+  colorScheme: ColorScheme.light(primary: Color(0xFFEF7822)),
+  textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(primary: Color(0xFFEF7822))),
+);
